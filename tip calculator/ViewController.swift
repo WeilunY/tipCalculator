86826fecf4acc36e7725d5b62104aa693ab1a7b6
Weilun Yao <yaoweilun1998@hotmail.com>
2019-01-13 20:12:05 -0800

    Initial Commit

---
+++ tip calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip calculator
+//
+//  Created by Weilun Yao on 1/13/19.
+//  Copyright © 2019 Weilun Yao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
